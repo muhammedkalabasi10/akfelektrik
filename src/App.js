43c7940580b1bcb738d6723cdfb5577084576d71
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-11-09 11:16:15 +0300

    browserrouter changed as hashrouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './index.css';
 import logojpg from './images/logojpg.jpg'
 import PhoneIcon from '@mui/icons-material/Phone';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
