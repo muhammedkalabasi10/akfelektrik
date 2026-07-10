b62c9b6dd0d52336e742aa28cf91f80f091df304
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2026-07-10 18:24:53 +0300

    Reapply "fix error"
    This reverts commit a3d4eee18fd3361abc2bf37801698b7d2eb3c3c2.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,7 +20,7 @@ const Hero = () => {
                             </h1>
                             <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">Anadolu yakasında 7/24 teknik servis ekiplerimiz hizmet vermektedir.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                                <a href="tel:05511348518" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0" onClick={() => gtag_report_conversion('tel:05511348518')}>
+                                <a href="tel:05511348518" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0" onClick={() => window.gtag_report_conversion?.('tel:05511348518')}>
                                     📞 Hemen Ara
                                     <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                         <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
